501dc7ed9ed7654bdedcc9e7dd4d7fbe937c5f57
chris <chrisliu.cs@outlook.com>
2023-09-02 00:43:19 +0800

    update plugins

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,27 +1,22 @@
-local F = {}
-
-F.default = function()
-  require("neo-tree").setup({
-    window = {
-      width = 30,
-    },
-  })
-end
-
 return {
-  {
-     "nvim-neo-tree/neo-tree.nvim",
-     branch = "v3.x",
-     dependencies = {
-       "nvim-lua/plenary.nvim",
-       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-       "MunifTanjim/nui.nvim",
-     },
-     keys = {
-       { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
-     },
-     config = function()
-       F.default()
-     end
-  }
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+		},
+		config = function()
+			require("neo-tree").setup({
+				window = {
+					width = 30,
+				},
+			})
+
+			-- set keymaps
+			local keymap = vim.keymap -- for conciseness
+			keymap.set("n", "<leader>ee", "<cmd>Neotree toggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explore      F.default()
+		end,
+	},
 }
